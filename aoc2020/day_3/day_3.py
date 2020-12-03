915f68db7224f88a8d32680c327ff333bb90fb24
Aurelien Busi <aurelien.busi@numberly.com>
2020-12-03 08:24:35 +0100

    new: add day3 solver

--- aoc2020/day_3/day_3.py
+++ aoc2020/day_3/day_3.py
@@ -1,2 +1,27 @@
+def stage_one(inp, sx=1, sy=3):
+    nb_tree = 0
+    y = 0
+    line_size = len(inp[0])
+    size_mountain = len(inp)
+    for x in range(0, size_mountain, sx):
+        if inp[x][y] == "#":
+            nb_tree = nb_tree + 1
+        y = (y + sy) % (line_size - 1)
+
+    return nb_tree
+
+
 def solve(inp):
-    pass
+
+    r1_3 = stage_one(inp)
+
+    print(f"One: {r1_3}")
+
+    r1_1 = stage_one(inp, 1, 1)
+    r1_5 = stage_one(inp, 1, 5)
+    r1_7 = stage_one(inp, 1, 7)
+    r2_1 = stage_one(inp, 2, 1)
+
+    r = r1_3 * r1_1 * r1_5 * r1_7 * r2_1
+
+    print(f"Two: {r}")
