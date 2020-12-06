18c77a86cd7c675427c03c1807ece0b844eeddb7
Aurelien Busi <aurelien.busi@numberly.com>
2020-12-06 08:29:28 +0100

    new: day 6 solver

--- aoc2020/day_6/day_6.py
+++ aoc2020/day_6/day_6.py
@@ -1,2 +1,26 @@
 def solve(inp):
-    pass
+    uniq = set()
+    al = set()
+    s1 = 0
+    s2 = 0
+    f = 1
+    for line in inp:
+        if line == "\n":
+            s1 += len(uniq)
+            s2 += len(al)
+            uniq = set()
+            al = set()
+            f = 1
+            continue
+        [uniq.add(x) for x in line[:-1]]
+
+        if f:
+            al = set(line[:-1])
+            f = 0
+        else:
+            al = al.intersection(set(line[:-1]))
+
+    s1 += len(uniq)
+    s2 += len(al)
+    print(f"One: {s1}")
+    print(f"Two: {s2}")
