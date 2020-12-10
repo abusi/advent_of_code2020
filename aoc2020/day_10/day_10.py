8b5e559cc73cb4e1329f032ffe0703fb7ccebc47
Aurelien Busi <aurelien.busi@numberly.com>
2020-12-10 14:45:58 +0100

    new: add day10 part 2 solver

--- aoc2020/day_10/day_10.py
+++ aoc2020/day_10/day_10.py
@@ -14,11 +14,26 @@ def stage1(ordered):
     return d1 * d3
 
 
+def stage2(ordered):
+    myplug = max(ordered) + 1
+    ways = [0] * myplug
+    ways[0] = 1
+    for i in range(1, myplug):
+        for x in range(1, 4):
+            if (i - x) in ordered:
+                ways[i] += ways[i - x]
+    return ways[-1]
+
+
 def solve(inp):
     inp = make_int(inp)
     s1, s2 = (0, 0)
 
     ordered = sorted(inp)
     s1 = stage1(ordered)
+
+    inp.append(0)
+    inp.append(max(inp) + 3)
+    s2 = stage2(sorted(inp))
 
     return s1, s2
