ae2463d8542f289b88367cee8ef1eae4ef0da867
Aurelien Busi <aurelien.busi@numberly.com>
2020-12-15 11:02:38 +0100

    new: add day15 solver

--- aoc2020/day_15/day_15.py
+++ aoc2020/day_15/day_15.py
@@ -1,4 +1,43 @@
+def stage1(spoken):
+    for i in range(len(spoken) + 1, 2020 + 1):
+        if spoken[-1] not in spoken[:-1]:
+            spoken.append(0)
+        else:
+            lspoken = len(spoken[:-1]) - 1 - spoken[:-1][::-1].index(spoken[-1]) + 1
+            spoken.append(i - 1 - lspoken)
+
+    return spoken[-1]
+
+
+def when_was_i_seen_before(indexes, x):
+    if len(indexes[x]) == 1:
+        return indexes[x][0]
+    return indexes[x][-2]
+
+
+def add_to_indexes(index, x, i):
+    if x not in index:
+        index[x] = []
+    index[x].append(i)
+
+
+def stage2(spoken):
+    index = {x: [i] for i, x in enumerate(spoken)}
+
+    for i in range(len(spoken), 30000000):
+        if (lspoken := when_was_i_seen_before(index, spoken[-1])) == i - 1:
+            spoken.append(0)
+            index[0].append(i)
+        else:
+            spoken.append(i - 1 - lspoken)
+            add_to_indexes(index, i - 1 - lspoken, i)
+
+    return spoken[-1]
+
+
 def solve(inp):
-    s1, s2 = (0, 0)
+    spoken1 = [int(x.strip()) for x in inp[0].split(",")]
+    spoken2 = list(spoken1)
+    s1, s2 = (stage1(spoken1), stage2(spoken2))
 
     return s1, s2
